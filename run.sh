247866eeb94599fea747841f591577d09bfce951
Mohamed Mansour <hello@mohamedmansour.com>
2012-11-10 00:14:46 -0800

    Organize the folder structure and include the proper libs.

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+DIR="$( cd "$( /usr/bin/dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Add debug command so we can pass break for debugging
+ARGS="--debug"
+if [ "$1" == "break" ]; then
+  echo "Added breakpoint to the first line, make sure you have node-inspector running!"
+  ARGS="--debug-brk"
+  shift
+fi
+
+# Force kill for devs.
+KILL_ALL_NODE=${KILL_ALL_NODE-0}
+if [ $KILL_ALL_NODE == 1 ]; then
+  /usr/bin/killall node
+fi
+
+# Start server
+cd $DIR
+node $ARGS $DIR/server/server.js $@
