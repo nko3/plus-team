3a87d01b29fe331d0017e8392c6f39b36756f6dd
abraham <4braham@gmail.com>
2012-11-10 01:40:47 -0800

    added auth redirect stubs

--- server/routes.js
+++ server/routes.js
@@ -7,6 +7,16 @@ exports.attach = function attachRoutes(app) {
   // Main landing page.
   app.resource(require('./controllers/index_controller'));
 
+  app.get('/auth/github', function(req, res) {
+    // TODO: full GitHub auth URL
+    res.redirect('https://github.com/login/oauth/authorize');
+  });
+  
+  app.get('/auth/facebook', function(req, res) {
+    // TODO: full Facebook auth URL
+    res.redirect('https://www.facebook.com/dialog/oauth');
+  });
+
   app.all('*', function(req, res) {
     res.render('layout');
   });
