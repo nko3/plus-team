92ce8fa52d9a363742153e2e81654ddecb725057
Rayan Bouajram <rbouajram@gmail.com>
2012-11-11 09:23:35 -0800

    Added Raphaeljs library; included new homepage screen

--- public/js/views/profile_view.js
+++ public/js/views/profile_view.js
@@ -6,6 +6,21 @@ GL.Views.Profile = GL.Framework.View.extend({
 
   render: function() {
     $(this.el).html(this._template());
+
+    var r = Raphael(10, 50, 640, 480);
+    this.render = r.piechart(
+    	0,
+    	0,
+    	70,
+    	[22, 22, 44, 12],
+    	{
+    		colors:['#EC992E','#3C35D8','#D83535','#DAD100'],
+	    	stroke:"#fff",
+	    	strokeWidth:1,
+
+    	}
+    );
+
     return this;
   }
 });
